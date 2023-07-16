5a49e3468c10b0bfcb8c03ef40427aec0e1ac92d
fabi8bit <fabi8bit@gmail.com>
2023-07-16 07:55:09 +0000

    add blog, create models, create superuser

--- bidepp/settings.py
+++ bidepp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'django_countries',
     'cloudinary',
     'blog',
 ]
@@ -133,7 +134,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=80)
+    country = CountryField(blank_label="(select country)")
+    city = models.CharField(max_length=30)
+    address = models.CharField(max_length=80)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    preferred = models.ManyToManyField(
+        User, related_name='preferred_hotel', blank=True)
+
+class Review(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_reviews"
+    )
+    hotel = models.ForeignKey(
+        Hotel, on_delete=models.CASCADE, related_name="blog_reviews"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(
+        User, related_name='review_like', blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
