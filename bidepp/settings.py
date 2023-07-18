ea41233cacdba3f44a03c982ef76f85d9f373ff2
fabi8bit <fabi8bit@gmail.com>
2023-07-18 09:07:28 +0000

    Add summernote editor for content field in Review model

--- bidepp/settings.py
+++ bidepp/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_countries',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review, Hotel
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+
+admin.site.register(Hotel)
