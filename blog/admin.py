127bc11fce1fc1897fb6f854d5d09c09fe234c06
fabi8bit <fabi8bit@gmail.com>
2023-07-18 10:48:16 +0000

    Register comments to admin page and add approve functionality for comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-from .models import Review, Hotel
+from .models import Review, Hotel, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'hotel', 'slug', 'status', 'created_on', 'author')
+    search_fields = ['title', 'content', 'hotel__name']
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'review', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
 
 
 admin.site.register(Hotel)
