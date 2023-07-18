9e0ab274146be1836878dcaea8e77778fd2fd388
fabi8bit <fabi8bit@gmail.com>
2023-07-18 13:34:27 +0000

    Create the view to display all the reviews in the blog

--- blog/admin.py
+++ blog/admin.py
@@ -23,5 +23,4 @@ class CommentAdmin(admin.ModelAdmin):
         queryset.update(approved=True)
 
 
-
 admin.site.register(Hotel)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Review
 
-# Create your views here.
+
+class ReviewList(generic.ListView):
+    model = Review
+    queryset = Review.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog.html'
+    paginate_by = 6
