1163fa2c0d60528f59254b3e16b1b9148a7072e4
fabi8bit <fabi8bit@gmail.com>
2023-07-29 06:33:06 +0000

    Adjust settings.py for deployment

--- bidepp/settings.py
+++ bidepp/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['bidepp-cc1716a9edf6.herokuapp.com','8000-fabi8bit-bideppfspp4-5aqbgymdmss.ws-eu102.gitpod.io']
 
